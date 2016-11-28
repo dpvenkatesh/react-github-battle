914b27df5d7080b7f78855a0f01627ce38e64939
Venkatesha D P <venkatesha.p@flipkart.com>
2016-11-29 00:59:49 +0530

    creating child components

--- app/index.js
+++ app/index.js
@@ -1,12 +1,52 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 
-var HelloWorld = React.createClass({
-    render: () => {
+var USER_DATA = {
+    name: "Venkatesh D P",
+    username: "dpvenkatesh",
+    image: "https://lh3.googleusercontent.com/BdU-H70Ar0-T_qw661a-Zo7kJVWaNIzLxgS9JiVOCA724N9WxaasdNKizCtu_W5wEdbIix2sUg=w6720-h4200-rw-no"
+};
+
+var ProfilePic = React.createClass({
+    render: function () {
+        return <img src={this.props.image} alt="profile picture" style={{height: 100, width: 100}}/>
+    }
+});
+
+
+var ProfileName = React.createClass({
+    render: function () {
         return (
-            <div> HelloWorld </div>
+            <div>
+                {this.props.name}
+            </div>
         )
     }
 });
 
-ReactDom.render(<HelloWorld />, document.getElementById('app'));
+var ProfileLink = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <a href={'https://www.github.com/' + this.props.username }> {this.props.username}</a>
+            </div>
+        )
+    }
+});
+
+var Avatar = React.createClass({
+
+    render: function () {
+        return (
+            <div>
+                <ProfilePic image={this.props.user.image}/>
+                <ProfileName name={this.props.user.name}/>
+                <ProfileLink username={this.props.user.username}/>
+            </div>
+        )
+    }
+});
+
+ReactDom.render(
+    <Avatar user={USER_DATA}/>, document.getElementById('app')
+);
